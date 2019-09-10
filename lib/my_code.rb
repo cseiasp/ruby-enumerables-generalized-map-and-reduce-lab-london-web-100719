91b9507a4c13463d6bfa4051a6ea904116093901
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-10 14:06:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,6 +8,7 @@ end
 array
 end 
 
+<<<<<<< HEAD
 def reduce(array, starting_value = nil)
   if starting_value 
     total = starting_value
@@ -16,9 +17,37 @@ def reduce(array, starting_value = nil)
     total = array[0]
     i=1
   end
+=======
+def reduce(array, starting_value = 0)
+  i=0 
+  if starting_value 
+  total = 0
+  total = total + starting_value
+else 
+  total = array[0]
+end
+>>>>>>> 54f2e814a2c32a446f2ea370e6ef46e1ef13e812
   while i < array.length do
   total = yield(total, array[i])
     i +=1 
   end
   total
 end
+<<<<<<< HEAD
+=======
+
+# def reduce(array, starting_value=nil)
+#   if starting_value
+#     total = starting_value
+#     i = 0
+#   else
+#     total = array[0]
+#     i = 1
+#   end
+#   while i < array.length
+#     total = yield(total, array[i])
+#     i += 1
+#   end
+#   total
+# end
+>>>>>>> 54f2e814a2c32a446f2ea370e6ef46e1ef13e812
